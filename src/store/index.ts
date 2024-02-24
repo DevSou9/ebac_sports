f77e3e1fe7f3d3d3f5c2ccb833beb49aa742b5b5
W Souza <dev9.ws@gmail.com>
2024-02-24 13:26:07 -0300

    reducers 2: carrinho e favoritos

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import carrinhoSlice from './carrinho'
 import api from '../service/api'
+import favoritosSlcie from './reducer/favoritos'
 export const store = configureStore({
   reducer: {
     carrinho: carrinhoSlice,
+    favoritos: favoritosSlcie,
     [api.reducerPath]: api.reducer
   },
   middleware: (getDefaultMiddleware) =>
